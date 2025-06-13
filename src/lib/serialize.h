canonical/dqlite

commit 451cff63b29366237a9502823299b05bbff8662b

Add tuple decoder

src/lib/serialize.h
@@ -13,6 +13,27 @@
  */
 #define SERIALIZE__WORD_SIZE 8
 
+/* We rely on the size of double to be 64 bit, since that's what is sent over
+ * the wire.
+ *
+ * See https://stackoverflow.com/questions/752309/ensuring-c-doubles-are-64-bits
+ */
+#ifndef __STDC_IEC_559__
+#if __SIZEOF_DOUBLE__ != 8
+#error "Requires IEEE 754 floating point!"
+#endif
+#endif
+#ifdef static_assert
+static_assert(sizeof(double) == sizeof(uint64_t),
+	      "Size of 'double' is not 64 bits");
+#endif
+
+/**
+ * Basic type aliases to used by macro-based processing.
+ */
+typedef const char *text_t;
+typedef double float_t;
+
 /**
  * Cursor to progressively read a buffer.
  */
@@ -190,6 +211,30 @@ DQLITE_INLINE int uint64__decode(struct cursor *cursor, uint64_t *value)
 		return DQLITE_PARSE;
 	}
 	*value = byte__flip64(*(uint64_t *)cursor->p);
+	cursor->p += n;
+	cursor->cap -= n;
+	return 0;
+}
+
+DQLITE_INLINE int int64__decode(struct cursor *cursor, int64_t *value)
+{
+	size_t n = sizeof(int64_t);
+	if (n > cursor->cap) {
+		return DQLITE_PARSE;
+	}
+	*value = byte__flip64(*(int64_t *)cursor->p);
+	cursor->p += n;
+	cursor->cap -= n;
+	return 0;
+}
+
+DQLITE_INLINE int float__decode(struct cursor *cursor, float_t *value)
+{
+	size_t n = sizeof(double);
+	if (n > cursor->cap) {
+		return DQLITE_PARSE;
+	}
+	*(uint64_t *)value = byte__flip64(*(uint64_t *)cursor->p);
 	cursor->p += n;
 	cursor->cap -= n;
 	return 0;

Makefile.am
@@ -52,6 +52,7 @@ libdqlite_la_SOURCES = \
   src/response.c \
   src/server.c \
   src/stmt.c \
+  src/tuple.c \
   src/tx.c \
   src/vfs.c
 include_HEADERS += include/dqlite.h
@@ -102,6 +103,7 @@ unit_test_SOURCES += \
   test/unit/test_leader.c \
   test/unit/test_registry.c \
   test/unit/test_replication.c \
+  test/unit/test_tuple.c \
   test/unit/main.c
 unit_test_CFLAGS = $(AM_CFLAGS)
 unit_test_CFLAGS += -I$(top_srcdir)/test

src/lib/byte.h
@@ -5,12 +5,6 @@
 #include <stdint.h>
 #include <string.h>
 
-/**
- * Basic type aliases to used by macro-based processing.
- */
-typedef const char *text_t;
-typedef double double_t;
-
 #if DQLITE_COVERAGE
 #define DQLITE_INLINE static inline
 #elif defined(__cplusplus) || \

src/tuple.c
@@ -1,0 +1,108 @@
+#include "tuple.h"
+#include "assert.h"
+
+int tuple_decoder__init(struct tuple_decoder *d,
+			unsigned n,
+			struct cursor *cursor)
+{
+	size_t header_size;
+	int rc;
+	d->is_row = n != 0;
+	if (d->is_row) {
+		d->n = n;
+	} else {
+		uint8_t byte;
+		rc = uint8__decode(cursor, &byte);
+		if (rc != 0) {
+			return rc;
+		}
+		d->n = byte;
+	}
+	d->i = 0;
+
+	/* Check that there is enough room to hold n type code slots. If the
+	 * tuple is a row, then each slot is 4 bits, otherwise if the tuple is a
+	 * sequence of parameters each slot is 8 bits. */
+	if (d->is_row) {
+		header_size = (n / 2) * sizeof(uint8_t);
+		if (n % 2 != 0) {
+			header_size += sizeof(uint8_t);
+		}
+		header_size = byte__pad64(header_size);
+	} else {
+		header_size = d->n * sizeof(uint8_t);
+		header_size = byte__pad64(header_size);
+		header_size -= sizeof(uint8_t); /* The first byte holds n */
+	}
+
+	if (header_size > cursor->cap) {
+		return DQLITE_PARSE;
+	}
+
+	d->header = cursor->p;
+	d->cursor = cursor;
+	d->cursor->p += header_size;
+	d->cursor->cap -= header_size;
+
+	return 0;
+}
+
+/* Return the type of the i'th value of the tuple. */
+static int get_type(struct tuple_decoder *d, unsigned i)
+{
+	int type;
+
+	/* In row format the type slot size is 4 bits, while in params format
+	 * the slot is 8 bits. */
+	if (d->is_row) {
+		type = d->header[i / 2];
+		if (d->i % 2 == 0) {
+			type &= 0x0f;
+		} else {
+			type = type >> 4;
+		}
+	} else {
+		type = d->header[i];
+	}
+
+	return type;
+}
+
+int tuple_decoder__next(struct tuple_decoder *d, struct value *value)
+{
+	int rc;
+	assert(d->i < d->n);
+	value->type = get_type(d, d->i);
+	switch (value->type) {
+		case SQLITE_INTEGER:
+			rc = int64__decode(d->cursor, &value->integer);
+			break;
+		case SQLITE_FLOAT:
+			rc = float__decode(d->cursor, &value->float_);
+			break;
+		case SQLITE_BLOB:
+			assert(0); /* TODO */
+			break;
+		case SQLITE_NULL:
+			/* TODO: allow null to be encoded with 0 bytes? */
+			rc = uint64__decode(d->cursor, &value->null);
+			break;
+		case SQLITE_TEXT:
+			rc = text__decode(d->cursor, &value->text);
+			break;
+		case DQLITE_ISO8601:
+			rc = text__decode(d->cursor, &value->iso8601);
+			break;
+		case DQLITE_BOOLEAN:
+			rc = uint64__decode(d->cursor, &value->boolean);
+			break;
+		default:
+			rc = DQLITE_PARSE;
+			break;
+	};
+	if (rc != 0) {
+		return rc;
+	}
+	d->i++;
+	return 0;
+}

src/tuple.h
@@ -1,0 +1,92 @@
+/**
+ * Encode and decode tuples of database values.
+ *
+ * A tuple is composed by a header and a body.
+ *
+ * The format of the header changes depending on whether the tuple is a sequence
+ * of parameters to bind to a statement, or a sequence of values of a single row
+ * yielded by a query.
+ *
+ * For a tutple of parameters the format of the header is:
+ *
+ *  8 bits: Number of values in the tuple.
+ *  8 bits: Type code of the 1st value of the tuple.
+ *  8 bits: Type code of the 2nd value of the tuple, or 0.
+ *  8 bits: Type code of the 3rd value of the tuple, or 0.
+ *  ...
+ *
+ * This repeats until reaching a full 64-bit word. If there are more than 7
+ * parameters to bind, the header will grow additional 64-bit words as needed,
+ * following the same pattern: a sequence of 8-bit slots with type codes of the
+ * parameters followed by a sequence of zero bits, until word boundary is
+ * reached.
+ *
+ * For a tuple of row values the format of the header is:
+ *
+ *  4 bits: Type code of the 1st value of the tuple.
+ *  4 bits: Type code of the 2nd value of the tuple, or 0.
+ *  4 bits: Type code of the 3rd value of the tuple, or 0.
+ *  ...
+ *
+ * This repeats until reaching a full 64-bit word. If there are more than 16
+ * values, the header will grow additional 64-bit words as needed, following the
+ * same pattern: a sequence of 4-bit slots with type codes of the values
+ * followed by a sequence of zero bits, until word boundary is reached.
+ *
+ * After the header the body follows immediately, which contains all parameters
+ * or values in sequence, encoded using type-specific rules.
+ */
+
+#ifndef DQLITE_TUPLE_H_
+#define DQLITE_TUPLE_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <unistd.h>
+
+#include "./lib/serialize.h"
+
+/**
+ * Hold a single database value.
+ */
+struct value
+{
+	int type;
+	union {
+		int64_t integer;
+		double float_;
+		uint64_t null;
+		const char *text;
+		const char *iso8601; /* Date */
+		uint64_t boolean;
+	};
+};
+
+/**
+ * Maintain state while decoding a single tuple.
+ */
+struct tuple_decoder
+{
+	bool is_row;	   /* Whether the tuple has row format */
+	unsigned n;	    /* Number of values in the tuple */
+	unsigned i;	    /* Index of next value to decode */
+	const uint8_t *header; /* Pointer to tuple header */
+	struct cursor *cursor; /* Reading cursor */
+};
+
+/**
+ * Initialize the state of the decoder, before starting to decode a new
+ * tuple. If @n is zero, it means that the tuple is a sequence of statement
+ * parameters. In that case the d->n field will be read from the first byte of
+ * @cursor.
+ */
+int tuple_decoder__init(struct tuple_decoder *d,
+			unsigned n,
+			struct cursor *cursor);
+
+/**
+ * Decode the next value of the tuple.
+ */
+int tuple_decoder__next(struct tuple_decoder *d, struct value *value);
+
+#endif /* DQLITE_TUPLE_H_ */

test/unit/test_tuple.c
@@ -1,0 +1,278 @@
+#include <sqlite3.h>
+
+#include "../../src/tuple.h"
+
+#include "../lib/runner.h"
+
+TEST_MODULE(tuple);
+
+/******************************************************************************
+ *
+ * Helper macros.
+ *
+ ******************************************************************************/
+
+#define DECODER_INIT(N)                                          \
+	{                                                        \
+		int rc2;                                         \
+		rc2 = tuple_decoder__init(&decoder, N, &cursor); \
+		munit_assert_int(rc2, ==, 0);                    \
+	}
+
+#define DECODER_NEXT                                         \
+	{                                                    \
+		int rc2;                                     \
+		rc2 = tuple_decoder__next(&decoder, &value); \
+		munit_assert_int(rc2, ==, 0);                \
+	}
+
+/******************************************************************************
+ *
+ * Assertions.
+ *
+ ******************************************************************************/
+
+#define ASSERT_VALUE_TYPE(TYPE) munit_assert_int(value.type, ==, TYPE)
+
+/******************************************************************************
+ *
+ * Decoder.
+ *
+ ******************************************************************************/
+
+TEST_SUITE(decoder);
+
+TEST_GROUP(decoder, init);
+
+/* If n is 0, then the prefix is used to dermine the number of elements of the
+ * tuple. */
+TEST_CASE(decoder, init, prefix, NULL)
+{
+	struct tuple_decoder decoder;
+	char buf[] = {2, 0, 0, 0, 0, 0, 0, 0, 0};
+	struct cursor cursor = {buf, sizeof buf};
+	(void)data;
+	(void)params;
+	DECODER_INIT(0);
+	munit_assert_int(decoder.n, ==, 2);
+	return MUNIT_OK;
+}
+
+/* If n is not 0, then it is the number of elements. */
+TEST_CASE(decoder, init, no_prefix, NULL)
+{
+	struct tuple_decoder decoder;
+	char buf[] = {2, 0, 0, 0, 0, 0, 0, 0, 0};
+	struct cursor cursor = {buf, sizeof buf};
+	(void)data;
+	(void)params;
+	DECODER_INIT(3);
+	munit_assert_int(decoder.n, ==, 3);
+	return MUNIT_OK;
+}
+
+TEST_GROUP(decoder, row);
+
+/* Decode the next tuple, with row format and only one value. */
+TEST_CASE(decoder, row, one_value, NULL)
+{
+	struct tuple_decoder decoder;
+	uint8_t buf[][8] = {
+	    {SQLITE_INTEGER, 0, 0, 0, 0, 0, 0, 0},
+	    {7, 0, 0, 0, 0, 0, 0, 0},
+	};
+	struct cursor cursor = {buf, sizeof buf};
+	struct value value;
+
+	(void)data;
+	(void)params;
+
+	DECODER_INIT(1);
+	DECODER_NEXT;
+
+	ASSERT_VALUE_TYPE(SQLITE_INTEGER);
+	munit_assert_int(value.integer, ==, 7);
+
+	return MUNIT_OK;
+}
+
+/* Decode the next tuple, with row format and two values. */
+TEST_CASE(decoder, row, two_values, NULL)
+{
+	struct tuple_decoder decoder;
+	uint8_t buf[][8] = {
+	    {SQLITE_INTEGER | SQLITE_TEXT << 4, 0, 0, 0, 0, 0, 0, 0},
+	    {7, 0, 0, 0, 0, 0, 0, 0},
+	    {'h', 'e', 'l', 'l', 'o', 0, 0, 0},
+	};
+	struct cursor cursor = {buf, sizeof buf};
+	struct value value;
+
+	(void)data;
+	(void)params;
+
+	DECODER_INIT(2);
+	DECODER_NEXT;
+
+	ASSERT_VALUE_TYPE(SQLITE_INTEGER);
+	munit_assert_int(value.integer, ==, 7);
+
+	DECODER_NEXT;
+
+	ASSERT_VALUE_TYPE(SQLITE_TEXT);
+	munit_assert_string_equal(value.text, "hello");
+
+	return MUNIT_OK;
+}
+
+TEST_GROUP(decoder, params);
+
+/* Decode the next tuple, with params format and only one value. */
+TEST_CASE(decoder, params, one_value, NULL)
+{
+	struct tuple_decoder decoder;
+	uint8_t buf[][8] = {
+	    {1, SQLITE_INTEGER, 0, 0, 0, 0, 0, 0},
+	    {7, 0, 0, 0, 0, 0, 0, 0},
+	};
+	struct cursor cursor = {buf, sizeof buf};
+	struct value value;
+
+	(void)data;
+	(void)params;
+
+	DECODER_INIT(0);
+	DECODER_NEXT;
+
+	ASSERT_VALUE_TYPE(SQLITE_INTEGER);
+	munit_assert_int(value.integer, ==, 7);
+
+	return MUNIT_OK;
+}
+
+/* Decode the next tuple, with params format and two values. */
+TEST_CASE(decoder, params, two_values, NULL)
+{
+	struct tuple_decoder decoder;
+	uint8_t buf[][8] = {
+	    {2, SQLITE_INTEGER, SQLITE_TEXT, 0, 0, 0, 0, 0},
+	    {7, 0, 0, 0, 0, 0, 0, 0},
+	    {'h', 'e', 'l', 'l', 'o', 0, 0, 0},
+	};
+	struct cursor cursor = {buf, sizeof buf};
+	struct value value;
+
+	(void)data;
+	(void)params;
+
+	DECODER_INIT(0);
+	DECODER_NEXT;
+
+	ASSERT_VALUE_TYPE(SQLITE_INTEGER);
+	munit_assert_int(value.integer, ==, 7);
+
+	DECODER_NEXT;
+
+	ASSERT_VALUE_TYPE(SQLITE_TEXT);
+	munit_assert_string_equal(value.text, "hello");
+
+	return MUNIT_OK;
+}
+
+TEST_GROUP(decoder, type);
+
+/* Decode a floating point number. */
+TEST_CASE(decoder, type, float, NULL)
+{
+	struct tuple_decoder decoder;
+	uint8_t buf[][8] = {
+	    {SQLITE_FLOAT, 0, 0, 0, 0, 0, 0, 0},
+	    {0, 0, 0, 0, 0, 0, 0, 0},
+	};
+	struct cursor cursor = {buf, sizeof buf};
+	struct value value;
+	double pi = 3.1415;
+
+	(void)data;
+	(void)params;
+
+	memcpy(buf[1], &pi, sizeof pi);
+	*(uint64_t *)buf[1] = byte__flip64(*(uint64_t *)buf[1]);
+
+	DECODER_INIT(1);
+	DECODER_NEXT;
+
+	ASSERT_VALUE_TYPE(SQLITE_FLOAT);
+	munit_assert_double(value.float_, ==, 3.1415);
+
+	return MUNIT_OK;
+}
+
+/* Decode a null value. */
+TEST_CASE(decoder, type, null, NULL)
+{
+	struct tuple_decoder decoder;
+	uint8_t buf[][8] = {
+	    {SQLITE_NULL, 0, 0, 0, 0, 0, 0, 0},
+	    {0, 0, 0, 0, 0, 0, 0, 0},
+	};
+	struct cursor cursor = {buf, sizeof buf};
+	struct value value;
+
+	(void)data;
+	(void)params;
+
+	DECODER_INIT(1);
+	DECODER_NEXT;
+
+	ASSERT_VALUE_TYPE(SQLITE_NULL);
+
+	return MUNIT_OK;
+}
+
+/* Decode a date string in ISO8601 format. */
+TEST_CASE(decoder, type, iso8601, NULL)
+{
+	struct tuple_decoder decoder;
+	uint8_t buf[5][8] = {
+	    {DQLITE_ISO8601, 0, 0, 0, 0, 0, 0, 0},
+	};
+	struct cursor cursor = {buf, sizeof buf};
+	struct value value;
+
+	(void)data;
+	(void)params;
+
+	strcpy((char *)buf[1], "2018-07-20 09:49:05+00:00");
+
+	DECODER_INIT(1);
+	DECODER_NEXT;
+
+	ASSERT_VALUE_TYPE(DQLITE_ISO8601);
+	munit_assert_string_equal(value.iso8601, "2018-07-20 09:49:05+00:00");
+
+	return MUNIT_OK;
+}
+
+/* Decode a boolean. */
+TEST_CASE(decoder, type, boolean, NULL)
+{
+	struct tuple_decoder decoder;
+	uint8_t buf[][8] = {
+	    {DQLITE_BOOLEAN, 0, 0, 0, 0, 0, 0, 0},
+	    {1, 0, 0, 0, 0, 0, 0, 0},
+	};
+	struct cursor cursor = {buf, sizeof buf};
+	struct value value;
+
+	(void)data;
+	(void)params;
+
+	DECODER_INIT(1);
+	DECODER_NEXT;
+
+	ASSERT_VALUE_TYPE(DQLITE_BOOLEAN);
+	munit_assert_int(value.boolean, ==, 1);
+
+	return MUNIT_OK;
+}

cowsql/cowsql

commit c0d7c99632ea2ee01066988708cbb41f335cbdc3

src/lib/serialize.h: don't define double as float_t
libuv with commit 85b526f makes uv.h include math.h for the definitions
of NAN/INFINITY. That header also defines the ISO C standard float_t
type. Now that that definition is in scope, the cowsql definition in
src/lib/serialize.h conflicts with it.

Fixes: 451cff63b29366237a9502823299b05bbff8662b
Closes: https://github.com/cowsql/cowsql/issues/35
Signed-off-by: Brahmajit Das <listout@listout.xyz>

src/lib/serialize.h
@@ -37,7 +37,7 @@ static_assert(sizeof(double) == sizeof(uint64_t),
  * Basic type aliases to used by macro-based processing.
  */
 typedef const char *text_t;
-typedef double float_t;
+typedef double cowsql_float;
 typedef uv_buf_t blob_t;
 
 /**
@@ -143,7 +143,7 @@ COWSQL_INLINE size_t int64__sizeof(const int64_t *value)
 	return sizeof(int64_t);
 }
 
-COWSQL_INLINE size_t float__sizeof(const float_t *value)
+COWSQL_INLINE size_t float__sizeof(const cowsql_float *value)
 {
 	(void)value;
 	return sizeof(double);
@@ -190,7 +190,7 @@ COWSQL_INLINE void int64__encode(const int64_t *value, void **cursor)
 	*cursor += sizeof(int64_t);
 }
 
-COWSQL_INLINE void float__encode(const float_t *value, void **cursor)
+COWSQL_INLINE void float__encode(const cowsql_float *value, void **cursor)
 {
 	*(uint64_t *)(*cursor) = ByteFlipLe64(*(uint64_t *)value);
 	*cursor += sizeof(uint64_t);
@@ -273,7 +273,7 @@ COWSQL_INLINE int int64__decode(struct cursor *cursor, int64_t *value)
 	return 0;
 }
 
-COWSQL_INLINE int float__decode(struct cursor *cursor, float_t *value)
+COWSQL_INLINE int float__decode(struct cursor *cursor, cowsql_float *value)
 {
 	size_t n = sizeof(double);
 	if (n > cursor->cap) {